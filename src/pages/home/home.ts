fdbe37a39bafb685434cfd9a79a0728b71d37803
Frederic Huguenin <fred76temp@gmail.com>
2019-01-06 01:12:55 +0100

    feat: add command to upload badge

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, ToastController } from 'ionic-angular';
+import chameleon from '../../../plugins/cordova-plugin-chameleon/www/chameleon';
+
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,96 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  public badgeArray: Uint8Array;
 
+  constructor(
+    public navCtrl: NavController,
+    private toastCtrl: ToastController,
+  ) {
+
+  }
+
+  /**************************************************************************************
+   *  Read Badge from Chameleon Device
+   *************************************************************************************/
+  readBadge() {
+
+  }
+
+  /**************************************************************************************
+   *  Upload Badge to Chameleon Device
+   *************************************************************************************/
+  uploadBadge() {
+    this.transferBadgeToCham();
+  }
+
+  /**************************************************************************************
+   * TRANSFER BADGE from APP to CHAMELEON
+   *************************************************************************************/
+  transferBadgeToCham() {
+    // initialize USB driver
+    chameleon.initialize(
+      null,
+      success => this.isPresent(),
+      error => this.presentToastError("Le driver n'a pas pu être initialisé. Transfert annulé.")
+    )
+  }
+
+  // test if usb is connected
+  private isPresent() {
+    chameleon.isPresent(
+      null,
+      success => this.sendBadge(),
+      error => {
+        this.presentToastError("Le Chameleon n'est pas connecté. Transfert annulé.");
+        this.closeUSB();
+      }
+    )
+  };
+
+  // send badge (préviously dowloaded and stored in this.badgeArray)
+  private sendBadge() {
+    chameleon.uploadArray(
+      this.badgeArray,
+      success => {
+        this.presentToastSuccess("Chargement du badge réussi");
+        this.closeUSB()
+      },
+      error => {
+        this.presentToastError("Le chargement n'a pu être effectué. Transfert annulé.");
+        this.closeUSB();
+      }
+    )
+  };
+
+  // close USB
+  private closeUSB() {
+    chameleon.shutdown();
+  }
+
+
+  /**************************************************************************************
+   * IHM functions for LOADING and MESSAGE display
+   *************************************************************************************/
+  presentToastSuccess(msg?: string) {
+    let toast = this.toastCtrl.create({
+      message: msg || "Ok",
+      position: "top",
+      cssClass: "toast valid",
+      duration: 2000
+    });
+    toast.present();
+  }
+
+  presentToastError(error?: string) {
+    let toast = this.toastCtrl.create({
+      message: error || "Ooops, une erreur est survenue.",
+      position: "top",
+      showCloseButton: true,
+      cssClass: "toast error",
+      closeButtonText: "Fermer"
+    });
+    toast.present();
   }
 
 }
